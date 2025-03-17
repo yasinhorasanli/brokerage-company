cb9c4d23fdab6032550a6400242243272b8bdd83
yasinhorasanli <yasinhorasanli@gmail.com>
2025-03-18 00:19:34 +0300

    Initial commit

---
+++ src/main/java/com/yasin/brokerage/BrokerageCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.brokerage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerageCompanyApplication.class, args);
+	}
+
+}
